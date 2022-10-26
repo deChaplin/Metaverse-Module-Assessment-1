89c8e0e9252b4b1cc874ff51cb789fa30a2fd979
kyle chaplin <kyle.chaplin@aol.co.uk>
2022-10-26 12:55:54 +0100

    Top half of body tracks with headset and controllers.

--- Assets/VRRig.cs
+++ Assets/VRRig.cs
@@ -2,22 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+
+public class VRMap
+{
+    public Transform vrTarget;
+    public Transform rigTarget;
+    public Vector3 trackingPositionOffset;
+    public Vector3 trackingRotationOffset;
+
+    public void map()
+    {
+        rigTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
+        rigTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+    }
+}
+
 public class VRRig : MonoBehaviour
 {
+    public float turnSmoothness;
+    public VRMap head;
+    public VRMap lefthand;
+    public VRMap rightHand;
+
     public Transform headConstraint;
     public Vector3 headBodyOffset;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         headBodyOffset = transform.position - headConstraint.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.position = headConstraint.position + headBodyOffset;
-        transform.forward = Vector3.ProjectOnPlace(headConstraint.up,Vector3.up).normalized;
+        transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized, Time.deltaTime * turnSmoothness);
+
+        head.map();
+        lefthand.map();
+        rightHand.map();
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # Metaverse-Module-Assessment-1
-
-Asset used for body - https://assetstore.unity.com/packages/3d/characters/robots/space-robot-kyle-4696
-
-Video being followed for rigging - https://www.youtube.com/watch?v=tBYl-aSxUe0
