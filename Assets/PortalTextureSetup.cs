325c8342dbaafda687b5306abfeff062e8531b19
kyle chaplin <kyle.chaplin@aol.co.uk>
2022-11-14 17:02:27 +0000

    Started creating portal

---
+++ Assets/PortalTextureSetup.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour
+{
+    public Camera camera1;
+    public Material cameraMat1;
+
+    public Camera camera2;
+    public Material cameraMat2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (camera2.targetTexture != null)
+        {
+            camera2.targetTexture.Release();
+        }
+        camera2.targetTexture = new RenderTexture(Screen.width, Screen.height, 23);
+        cameraMat2.mainTexture = camera2.targetTexture;
+
+        if (camera1.targetTexture != null)
+        {
+            camera1.targetTexture.Release();
+        }
+        camera1.targetTexture = new RenderTexture(Screen.width, Screen.height, 23);
+        cameraMat1.mainTexture = camera1.targetTexture;
+    }
+}
